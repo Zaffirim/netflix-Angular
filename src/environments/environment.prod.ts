bc0941cb27bf171790f2ab4a1357e4281caa8d85
Milo <zaffirim@gmail.com>
2024-04-07 23:02:14 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseURL: 'http://localhost:3000/',
+  imageURL: 'https://image.tmdb.org/t/p/w500'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+
+export const environment = {
+  production: false,
+  baseURL: 'http://localhost:3000/',
+  imageURL: 'https://image.tmdb.org/t/p/w500'
+};
+
+
